4e9f49b561c7ac3744d93a480a5215fc7857da57
Delakroa <katon1986@gmail.com>
2021-02-20 13:07:29 +0300

    нужно разобраться

--- Vvod_dannih_v_spisok_while_chapter7.py
+++ Vvod_dannih_v_spisok_while_chapter7.py
@@ -399,4 +399,17 @@
 
 # Заполнение словаря данными, введенными пользователем
 
+responses = {}
 
+polling_activate = True  # установить флаг продолжения опроса.
+
+while polling_activate:
+    name = input("\nКак твоё имя? ")  # Запрос имени и ответа пользователя.
+    responses = input("На какую гору ты когда-нибудь захочешь подняться? ")
+    responses[name] = responses  # Ответ сохраняется в словаре:
+    repeat = input("Вы бы хотели, чтобы другой человек ответил? (yes/no) ")
+    if repeat == 'no':  # Проверка продолжения опроса.
+        polling_activate = False
+    print("\n---Poll Results---")  # Опрос завершён, вывести результат.
+    for name, response in responses:
+        print(name + " would like to climb " + responses + ".")
