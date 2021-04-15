dc96e7e2b443902de3a316b239d615123bad2eff
Delakroa <katon1986@gmail.com>
2021-04-15 13:31:47 +0300

    упражняемся в тестирование

--- test_name_function.py
+++ test_name_function.py
@@ -26,6 +26,11 @@ class NamesTestCase(unittest.TestCase):  # 1
         formatted_name = get_formatted_name('janis', 'joplin')  # 2
         self.assertEqual(formatted_name, 'Janis Joplin')  # 3
 
+    def test_first_last_middle_name(self):
+        """Работают ли такие имена, как 'Wolfgang Amadeus Mozart ?'"""
+        formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 
 if __name__ == '__main__':
     unittest.main()
