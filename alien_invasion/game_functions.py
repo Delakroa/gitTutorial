40ee3973cb4974815a5a8adc128199a8aa3b7e85
Delakroa <katon1986@gmail.com>
2021-04-22 23:08:07 +0300

    полный фарш

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -3,11 +3,22 @@ import sys
 import pygame
 
 
-def check_events():
-    # Отслеживание событий клавиатуры и мыши.
+def check_events(ship):
+    """Обрабатывает нажатия клавиш и события мыши."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
