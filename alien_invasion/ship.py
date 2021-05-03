35ee7f4d3a61d423d7899e95d27ee43e1821a423
delakroa <katon1986@gmail.com>
2021-05-03 12:38:31 +0300

    исправил

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -3,19 +3,22 @@ import pygame
 
 class Ship:
 
-    def __init__(self, ai_settings, screen):
+    def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
-        self.screen = screen
-        self.ai_settings = ai_settings
-        self.screen_rect = screen.get_rect()
+        self.screen = ai_game.screen
+        self.ai_settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
         # Загрузка изображения корабля и получение прямоугольника.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         # Каждый новый корабль появляется в центре нижнего края экрана.
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.midbottom = self.screen_rect.midbottom
+
         # Сохранение вещественной координаты цунтра коробля.
-        self.center = float(self.rect.centerx)
+        self.x = float(self.rect.x)
+
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
@@ -24,12 +27,12 @@ class Ship:
         """Обновляет позицию корабля с учётом флага."""
         # Обновляется атрибуты center, не rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed
+            self.x += self.ai_settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed
+            self.x -= self.ai_settings.ship_speed
 
             # Обновление обьекта rect на self.center.
-            self.rect.centerx = self.center
+            self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
