42864016a495338b433cd145ec5aae1fa02c8257
delakroa <katon1986@gmail.com>
2021-05-03 12:36:29 +0300

    исправил

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -5,26 +5,24 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """Класс для управления пулями, выпущенными кораблём"""
 
-    def __init__(self, ai_settings, screen, ship):
+    def __init__(self, ai_game):
         """Создаёт обьект пули в текущей позиции корабля."""
         super().__init__()
-        self.screen = screen
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
 
         # Создание пули в позиции (0,0) и назначение правильной позиции.
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
-        self.rect.centerx = ship.rect.centerx
-        self.rect.top = ship.rect.top
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
 
         # Позиция пули хранится в вещественном формате.
         self.y = float(self.rect.y)
 
-        self.color = ai_settings.bullet_color
-        self.speed_factor = ai_settings.bullet_speed_factor
-
     def update(self):
         """Перемещает пулю вверх по экрану."""
         # Обновление позиции пули в вещественном формате.
-        self.y -= self.speed_factor
+        self.y -= self.settings.bullet_speed
         # Обновление пзиции прямоугольника.
         self.rect.y = self.y
 
