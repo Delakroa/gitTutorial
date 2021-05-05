1d16b35455a109c49444037c7627a1787f14bf97
Delakroa <katon1986@gmail.com>
2021-05-05 07:52:33 +0300

    Создали пришельца и отрисовали его

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -1,14 +1,15 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Alien (Sprite):
+
+class Alien(Sprite):
     """Класс, представляющий одного пришельца."""
 
-    def __init__(self, ai_game):
+    def __init__(self, ai_settings, screen):
         """Инициализирует пришельца и задает его начальную позицию."""
         super().__init__()
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings
+        self.screen = screen
+        self.ai_settings = ai_settings
 
         # Загрузите изображение пришельца и установите его атрибут rect.
         self.image = pygame.image.load('images/alien.bmp')
@@ -18,16 +19,20 @@ class Alien (Sprite):
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        # Сохраните точное горизонтальное положение инопланетянина.
+        # Сохранение точной позиции пришельца.
         self.x = float(self.rect.x)
 
-    def check_edges(self):
-        """Верните True, если пришелец находится на краю экрана."""
-        screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
-            return True
+    def blitme(self):
+        """Выводит пришельца"""  # Отрисовка выполняется с помощью метода blit
+        self.screen.blit(self.image, self.rect)  # (можно создавать дополнительные поверхности)
 
-    def update(self):
-        """Переместите инопланетянина вправо или влево."""
-        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
-        self.rect.x = self.x
+    # def check_edges(self):
+    #     """Верните True, если пришелец находится на краю экрана."""
+    #     screen_rect = self.screen.get_rect()
+    #     if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+    #         return True
+    #
+    # def update(self):
+    #     """Переместите инопланетянина вправо или влево."""
+    #     self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+    #     self.rect.x = self.x
