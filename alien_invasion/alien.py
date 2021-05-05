5751de52b2e00e878777b73e3cc52f5bab5597c0
Delakroa <katon1986@gmail.com>
2021-05-05 14:20:13 +0300

    Проверка достижения края

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -28,14 +28,15 @@ class Alien(Sprite):
         # (можно создавать дополнительные поверхности)
         self.screen.blit(self.image, self.rect)
 
+    def check_edges(self):
+        """Верните True, если пришелец находится на краю экрана."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
     def update(self):
         """Переместите пришельца вправо."""
         self.x += self.ai_settings.alien_speed_factor
         self.rect.x = self.x
-
-    # def check_edges(self):
-    #     """Верните True, если пришелец находится на краю экрана."""
-    #     screen_rect = self.screen.get_rect()
-    #     if self.rect.right >= screen_rect.right or self.rect.left <= 0:
-    #         return True
-    #
