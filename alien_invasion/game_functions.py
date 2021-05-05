995e5929a86f9eec63ac4d83daa2387984796715
Delakroa <katon1986@gmail.com>
2021-05-05 14:42:53 +0300

    снижение флота и направления

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -108,6 +108,23 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     return number_rows
 
 
-def update_aliens(aliens):
-    """Обновляет позицию всех пришельцев во флоте."""
+def update_aliens(ai_settings, aliens):
+    """Проверяет, достиг ли флот края экрана,
+после чего обновляет позиции всех пришельцев во флоте."""
+    check_fleet_edges(ai_settings, aliens)
     aliens.update()
+
+
+def check_fleet_edges(ai_settings, aliens):
+    """Реагирует на достижение пришельцем края экрана"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """Опускает весь флот и меняет направление флота."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+        ai_settings.fleet_direction *= -1
