befd680cc104a12931dfc529bab84d3c07037668
Delakroa <katon1986@gmail.com>
2021-05-05 13:52:17 +0300

    Добавление рядов  def get_number_rows

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -78,21 +78,32 @@ def get_number_aliens_x(ai_settings, alien_width):
     return number_aliens_x
 
 
-def create_alien(ai_settings, screen, aliens, alien_number):
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     """Создание пришельца и размещает его в ряду."""
     alien = Alien(ai_settings, screen)  # Создание пришельцев и размер его ряда.
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
 
-def create_fleet(ai_settings, screen, aliens):
+def create_fleet(ai_settings, screen, ship, aliens):
     """Создаёт флот пришельцев."""
     # Создаёт пришельцев и вычисление количества пришельцев в ряду
     alien = Alien(ai_settings, screen)
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
 
-    # Создание первого ряда пришельцев.
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings, screen, aliens, alien_number)
+    # Создание флота пришельцев.
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """Определение количества рядов, помещающихся на экаран."""
+    available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
