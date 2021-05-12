f11bc9310e88d5fd8e2d30955a8d81e19ff571ee
Delakroa <katon1986@gmail.com>
2021-05-12 10:19:03 +0300

    Игра Alien Invasion запускается в неактивном состоянии

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,4 +1,4 @@
-class GameStats():
+class GameStats:
     """Отслеживание статистики для игры Alien Invasion."""
 
     def __init__(self, ai_settings):
@@ -12,4 +12,3 @@ class GameStats():
     def reset_stats(self):
         """Инициализирует статистику. изменяющуюся в ходе игры."""
         self.ships_left = self.ai_settings.ship_limit
-
