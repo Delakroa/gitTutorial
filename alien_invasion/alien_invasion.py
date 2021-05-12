c7dff5eb79175652551f5e976dda4d5a29356e35
Delakroa <katon1986@gmail.com>
2021-05-12 10:17:58 +0300

    создаём кнопку прямо в файле alien_invasion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,8 @@ import game_functions as gf
 from settings import Settings
 from ship import Ship
 from game_stats import GameStats
+from button import Button
+
 
 # from alien import Alien
 
@@ -15,6 +17,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Создание кнопки Play
+    play_button = Button(ai_settings, screen, "Play")
 
     # Создание экземпляра для хранения игровой статистики.
     stats = GameStats(ai_settings)
@@ -32,12 +37,12 @@ def run_game():
 
     # Запуск основного цикла игры.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
             gf.check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
 
 
