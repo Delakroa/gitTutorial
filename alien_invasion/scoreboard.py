25379452e5d9963b612f73d2a57d1c06a6ca257c
Delakroa <katon1986@gmail.com>
2021-05-17 23:05:20 +0300

    Исправил ошибку

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -21,7 +21,7 @@ class Scoreboard:
     def prep_score(self):
         """Преобразует текущий счёт в графическое изображение."""
         score_str = str(self.stats.score)
-        self.score_image = self.font.render(score_str, True, self, text_color, self.ai_settings.bg_color)
+        self.score_image = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
 
         # Выводит счёт в правой верхней части экрана.
         self.score_rect = self.score_image.get_rect()
