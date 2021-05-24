1e086efbe1a59aa88b1d7b6d10ee407a6394d585
Delakroa <katon1986@gmail.com>
2021-05-24 12:24:47 +0300

    Дописывание атрибутов

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -38,10 +38,10 @@ def run_game():
         gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
             gf.update_aliens(ai_settings, screen, stats, ship, aliens, bullets)
             gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
-            gf.check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+            gf.check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
 
 run_game()
