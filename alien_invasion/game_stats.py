c1890ccbfffe0ecc97961d1da422edf4dfd12d29
Delakroa <katon1986@gmail.com>
2021-05-24 13:13:51 +0300

    Рекорд не должен сбрасываться

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.ai_settings = ai_settings
         self.reset_stats()
 
+        # Рекорд не должен сбрасываться.
+        self.high_score = 0
+
         # Игра Alien Invasion запускается в неактивном состоянии.
         self.game_active = False
 
