e6418d0457d60042548b2304e6102ddba9756f51
Delakroa <katon1986@gmail.com>
2021-06-04 13:36:42 +0300

    Финал

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -205,7 +205,7 @@ class AlienInvasion:
         # Определите количество рядов пришельцев, которые умещаются на экране.
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height -
-                             (3 * alien_height) - ship_height)
+                             (8 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
 
         # Создайте полный флот инопланетян.
