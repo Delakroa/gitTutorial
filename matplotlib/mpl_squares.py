1e5c71b133b042114ba93d48619e695ccbac0799
Delakroa <katon1986@gmail.com>
2021-06-07 09:48:19 +0300

    Изменение типа надписей и толщины графика

--- matplotlib/mpl_squares.py
+++ matplotlib/mpl_squares.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# Назначение загаловка диагрыммы и меток осей.
+plt.title("Квадратные числа", fontsize=24)
+plt.xlabel("Значение", fontsize=14)
+plt.ylabel("Значение Квадрата", fontsize=14)
+# Назначение размера шрифта делений на осях.
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
