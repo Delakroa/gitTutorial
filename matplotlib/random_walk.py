f31853afe111edb3cbc5ce056d15dc6706aad388
Delakroa <katon1986@gmail.com>
2021-06-07 14:43:29 +0300

    нужно доделать

--- matplotlib/random_walk.py
+++ matplotlib/random_walk.py
@@ -11,3 +11,27 @@ class RandomWalk:
         # Все блуждания начинаются с точки (0, 0).
         self.x_value = [0]
         self.y_value = [0]
+
+    def fill_walk(self):
+        """Вычисляет все точки блуждания."""
+        # Шаги генерируются до достижения нужной длины.
+        while len(self.x_value) < self.num_points:
+            # определяет направление и длины перемещения
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+            y_direction = choice([1, -1])
+            y_distance = choice(([0, 1, 2, 3, 4]))
+            y_step = y_direction * y_distance
+
+            # Отклонение нулевых перемещений
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Вычисление следующих значений x и y
+            next_x = self.x_value[-1] + x_step
+            next_y = self.y_value[-1] + y_step
+
+            self.x_value.append(next_x)
+            self.y_value.append(next_y)
+
