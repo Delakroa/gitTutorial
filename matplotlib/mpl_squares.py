9e1152982f5484433474fe48da2e14bd37c86f46
Delakroa <katon1986@gmail.com>
2021-06-07 09:56:28 +0300

    Корректировка графика

--- matplotlib/mpl_squares.py
+++ matplotlib/mpl_squares.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
+input_value = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+plt.plot(input_value, squares, linewidth=5)
 
 # Назначение загаловка диагрыммы и меток осей.
 plt.title("Квадратные числа", fontsize=24)
-plt.xlabel("Значение", fontsize=14)
+plt.xlabel("Значения", fontsize=14)
 plt.ylabel("Значение Квадрата", fontsize=14)
 # Назначение размера шрифта делений на осях.
 plt.tick_params(axis='both', labelsize=14)
