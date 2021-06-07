eca54c78d051becc4aaf075c12d4e4bad6670805
Delakroa <katon1986@gmail.com>
2021-06-07 11:28:52 +0300

    Вывод серии точек функцией scatter()

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=100)
 # Назначение заголовка диагрыммы и меток осей.
-plt.title("Номер квадрата", fontsize=24)
-plt.xlabel("Значения", fontsize=14)
-plt.ylabel("Значение квадрата", fontsize=14)
+plt.title("Номер квадрата", fontsize=24)  # .title подписать загаловок.
+plt.xlabel("Значения", fontsize=14)  # Подписать ось Х.
+plt.ylabel("Значение квадрата", fontsize=14) # Подписать ось y.
 
 # Назначение размера шрифта на осях.
 plt.tick_params(axis='both', which='major', labelsize=14)
