f3258fa024f99072651654ddcd2a6fa4f3947f43
Delakroa <katon1986@gmail.com>
2021-06-08 14:01:16 +0300

    Построение гистограммы

--- Pygal/die_visual.py
+++ Pygal/die_visual.py
@@ -1,3 +1,5 @@
+import pygal
+
 from die import Die
 
 # Создание кубика D6.
@@ -14,6 +16,18 @@ frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# Визуализация результатов.
+hist = pygal.Bar()
+
+hist.title = 'Результаты броска одного кубика D6 1000 раз.'
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = 'Результат'
+hist.y_title = 'Частота результатов'
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+
 
 print(frequencies)
 
