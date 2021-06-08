0ed38788759a3f76ff96bb42a5f826a65bec794d
Delakroa <katon1986@gmail.com>
2021-06-08 09:53:44 +0300

    Назначение цветов

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -7,7 +7,8 @@ while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_value, rw.y_value, s=15)
+    point_number = list(range(rw.num_points))
+    plt.scatter(rw.x_value, rw.y_value, c=point_number, cmap=plt.cm.Blues, edgecolors='none', s=15)
     plt.show()
     keep_running = input("Сделайте еще одну Random Walk? (y/n): ")
     if keep_running == 'n':
