df82fbdb595caf8d4ec994987f27caafae19d726
Delakroa <katon1986@gmail.com>
2021-06-08 13:29:54 +0300

    Анализ результатов

--- Pygal/die_visual.py
+++ Pygal/die_visual.py
@@ -5,8 +5,16 @@ die = Die()
 
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
-for roll in range(100):
+for roll in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Анализ результов.
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
+
+
