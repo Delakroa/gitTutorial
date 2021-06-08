7203374e68611be67fc41c74a24aa6462ff8794b
Delakroa <katon1986@gmail.com>
2021-06-08 11:28:22 +0300

    Начальные и конечные точки

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -9,6 +9,9 @@ while True:
     rw.fill_walk()
     point_number = list(range(rw.num_points))
     plt.scatter(rw.x_value, rw.y_value, c=point_number, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    # Выделение первой и последней точки.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_value[-1], rw.y_value[-1], c='red', edgecolors='none', s=100)
     plt.show()
     keep_running = input("Сделайте еще одну Random Walk? (y/n): ")
     if keep_running == 'n':
