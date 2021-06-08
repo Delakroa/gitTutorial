74ebbe3b20c7ac7a59ba57f192939e2b80b5d31c
Delakroa <katon1986@gmail.com>
2021-06-08 12:03:15 +0300

    Удаление осей закоментированно. Выдаёт ошибку

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -12,6 +12,11 @@ while True:
     # Выделение первой и последней точки.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_value[-1], rw.y_value[-1], c='red', edgecolors='none', s=100)
+
+    # Удаление осей.
+    # plt.axes.get_xaxis().set_visible(False)
+    # plt.axes.get_yaxis().set_visible(False)
+
     plt.show()
     keep_running = input("Сделайте еще одну Random Walk? (y/n): ")
     if keep_running == 'n':
