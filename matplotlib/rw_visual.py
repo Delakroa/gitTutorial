f29a6f5f724b7908bbf5785ce6df93c19c1c36be
Delakroa <katon1986@gmail.com>
2021-06-08 09:09:02 +0300

    Генерирование нескольких случайных блужданий

--- matplotlib/rw_visual.py
+++ matplotlib/rw_visual.py
@@ -2,8 +2,13 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Построение случайного блуждания и нанесение точек на диаграмму.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_value, rw.y_value, s=15)
-plt.show()
+# Новое блуждания строятся до тех пор, пока программа остается активной.
+while True:
+    # Построение случайного блуждания и нанесение точек на диаграмму.
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_value, rw.y_value, s=15)
+    plt.show()
+    keep_running = input("Сделайте еще одну Random Walk? (y/n): ")
+    if keep_running == 'n':
+        break
