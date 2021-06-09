34f4bf966503137aa5f1567c033d5d3d37ea8ab1
Delakroa <katon1986@gmail.com>
2021-06-09 14:41:58 +0300

    исправление

--- alien_invasion/ai_player.py
+++ alien_invasion/ai_player.py
@@ -20,31 +20,40 @@ class AIPlayer:
 
         # Запустить основной цикл игры.
         while True:
-            # По-прежнему вызываем ai_game._check_events (), чтобы мы могли использовать клавиатуру для выхода.
+            # По-прежнему вызываем ai_game._check_events (), поэтому мы можем использовать клавиатуру для выхода.
+            # Также вызываем наш собственный метод для инициирования событий.
             self.ai_game._check_events()
-
-            # Непрерывно прокручивайте корабль вправо и влево.
-            ship = self.ai_game.ship
-            screen_rect = self.ai_game.screen.get_rect()
-
-            if not ship.moving_right and not ship.moving_left:
-                # Корабль еще не двинулся; двигаться вправо.
-                ship.moving_right = True
-            elif (ship.moving_right and ship.rect.right > screen_rect.right - 10):
-                # Корабль вот-вот ударится по правому краю; двигай влево.
-                ship.moving_right = False
-                ship.moving_left = True
-            elif ship.moving_left and ship.rect.left < 10:
-                ship.moving_left = False
-                ship.moving_right = True
+            self._implement_strategy()
 
             if self.ai_game.stats.game_active:
                 self.ai_game.ship.update()
                 self.ai_game._update_bullets()
                 self.ai_game._update_aliens()
-                self.ai_game._fire_bullet()
 
             self.ai_game._update_screen()
+
+    def _implement_strategy(self):
+        """Реализуйте автоматизированную стратегию игры."""
+        self._sweep_right_left()
+
+        # По возможности стреляйте пулей.
+        self.ai_game._fire_bullet()
+
+    def _sweep_right_left(self):
+        """Непрерывно прокручивайте корабль вправо и влево."""
+        ship = self.ai_game.ship
+        screen_rect = self.ai_game.screen.get_rect()
+
+        if not ship.moving_right and not ship.moving_left:
+            # Корабль еще не двинулся; двигаться вправо.
+            ship.moving_right = True
+        elif (ship.moving_right and ship.rect.right > screen_rect.right - 10):
+            # Корабль вот-вот ударится по правому краю; двигай влево.
+            ship.moving_right = False
+            ship.moving_left = True
+        elif ship.moving_left and ship.rect.left < 10:
+            ship.moving_left = False
+            ship.moving_right = True
 
 
 if __name__ == '__main__':
