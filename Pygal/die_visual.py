7f8c9a24f829dab3b7f8bf136d752801bd93e4c3
Delakroa <katon1986@gmail.com>
2021-06-09 08:01:15 +0300

    Бросок двух кубиков

--- Pygal/die_visual.py
+++ Pygal/die_visual.py
@@ -3,29 +3,31 @@ import pygal
 from die import Die
 
 # Создание кубика D6.
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
 for roll in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Анализ результов.
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Визуализация результатов.
 hist = pygal.Bar()
 
-hist.title = 'Результаты броска одного кубика D6 1000 раз.'
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
-hist.x_title = 'Результат'
+hist.title = 'Результаты броска двух кубиков D6 1000 раз.'
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = 'Результаты'
 hist.y_title = 'Частота результатов'
 
-hist.add('D6', frequencies)
+hist.add('D6 + D6', frequencies)
 hist.render_to_file('die_visual.svg')
 
 
