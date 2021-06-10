45451e3952801e663b5c4102cb0db5b2e9dc4dc1
Delakroa <katon1986@gmail.com>
2021-06-10 14:02:39 +0300

    Нанесение данных на диаграмму

--- CSV & JSON/highs_lows.py
+++ CSV & JSON/highs_lows.py
@@ -1,5 +1,8 @@
 import csv
 
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as pl
+
 # Чтение максимальных температур из файла.
 filename = 'D:\Python library\gitTutorial\CSV & JSON\data\death_valley_2018_full.csv'
 with open(filename, 'r') as f:
@@ -11,4 +14,16 @@ with open(filename, 'r') as f:
         high = int(row[6])
         highs.append(high)
 
-print(highs)
+# Нанесение данных на диаграмму.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# Фоматирование диаграммы.
+plt.title("Ежедневная температура, 06-01-2018")
+plt.xlabel('')
+plt.ylabel("Температура (F)")
+plt.tick_params(axis='both', which='major')
+
+plt.show()
+
+
