0e629764d98fa1f22293ef0f4eda574674feb2b0
Delakroa <katon1986@gmail.com>
2021-06-10 14:15:48 +0300

    current_date = datetime.strptime(row[2], "%Y-%m-%d") ошибка тут

--- CSV & JSON/highs_lows.py
+++ CSV & JSON/highs_lows.py
@@ -1,26 +1,32 @@
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 from matplotlib import pyplot as pl
 
-# Чтение максимальных температур из файла.
+# Чтение дат и температурных максимумов из файла.
 filename = 'D:\Python library\gitTutorial\CSV & JSON\data\death_valley_2018_full.csv'
 with open(filename, 'r') as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
-        high = int(row[6])
-        highs.append(high)
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(current_date)
+
+    high = int(row[6])
+    highs.append(high)
 
 # Нанесение данных на диаграмму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # Фоматирование диаграммы.
-plt.title("Ежедневная температура, 06-01-2018")
+plt.title("Ежедневная температура, 2018-01-06")
 plt.xlabel('')
+
+fig.autofmt_xdate()
 plt.ylabel("Температура (F)")
 plt.tick_params(axis='both', which='major')
 
