fe9793accd8e81f9bcbc0557ae34a10a9af48b3e
Delakroa <katon1986@gmail.com>
2021-06-10 12:44:01 +0300

    Печать заголовков и их позиций

--- CSV & JSON/highs_lows.py
+++ CSV & JSON/highs_lows.py
@@ -4,4 +4,7 @@ filename = 'D:\Python library\gitTutorial\CSV & JSON\data\death_valley_2018_full
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
