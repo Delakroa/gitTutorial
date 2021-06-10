2659def90f89a609463fda6ee1a89a1c4fd7255f
Delakroa <katon1986@gmail.com>
2021-06-10 11:58:37 +0300

    переместил

--- pizza.py
+++
@@ -1,8 +0,0 @@
-def make_pizza(size, *toppings):
-    """Выводит описание пиццы."""
-    print("\nСоздание " + str(size) + "-дюймовой пицци со следующими начинками:")
-    for topping in toppings:
-        print("- " + topping)
-
-
-make_pizza(46, 'колбаса')
