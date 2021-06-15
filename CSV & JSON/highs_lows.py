50cde3ec0243ccb2a342a8f0fa49fa56d46a73b9
Delakroa <katon1986@gmail.com>
2021-06-15 23:21:54 +0300

    Расширение временного диапазона

--- CSV & JSON/highs_lows.py
+++ CSV & JSON/highs_lows.py
@@ -11,19 +11,24 @@ with open(filename, 'r') as f:
     header_row = next(reader)
 
     # Получите даты и высокие температуры из этого файла.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%d-%m-%Y")
         dates.append(current_date)
+
         high = int(row[6])
         highs.append(high)
+
+        low = int(row[7])
+        lows.append(low)
 
 # Нанесение данных на диаграмму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs,  c='red')
+plt.plot(dates, lows, c='blue')
 
 # Фоматирование диаграммы.
-plt.title("Ежедневная температура, 06-01-2018", fontsize=24)
+plt.title("Ежедневные высокие и низкие температуры, 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 
 fig.autofmt_xdate()
