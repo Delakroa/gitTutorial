de7c7a042acdb36f673f0a8bf79d3b6653080106
delakroa <katon1986@gmail.com>
2021-09-09 21:02:02 +0300

    Автокликер

--- autoclicer.py
+++ autoclicer.py
@@ -1,11 +1,11 @@
 import pyautogui as pg
 
-pg.moveTo(115, 931, 0.5)
+pg.moveTo(115, 931, 2)
 
 # print(pg.position())  # Shift+F10
 
 pg.doubleClick(115, 931)
-pg.moveTo(588, 600, 0.5)
+pg.moveTo(588, 600, 2)
 pg.PAUSE = 3.5
 pg.typewrite('Volodia zavtra lomaem pentagon!\n')
 pg.typewrite('effectivnost nasha))\n')
