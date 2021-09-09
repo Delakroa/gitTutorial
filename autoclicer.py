574cde3cc50cfe198e727abb821263bcd855208b
delakroa <katon1986@gmail.com>
2021-09-09 20:05:27 +0300

    Автокликер

---
+++ autoclicer.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+
+pg.moveTo(115, 931, 0.5)
+
+# print(pg.position())  # Shift+F10
+
+pg.doubleClick(115, 931)
+pg.moveTo(588, 600, 0.5)
+pg.PAUSE = 3.5
+pg.typewrite('Volodia zavtra lomaem pentagon!\n')
+pg.typewrite('effectivnost nasha))\n')
+# pg.typewrite('Zaiushenka ia tebia ochen lublu!\n Eto soobshenie avtomaticheskoe')
