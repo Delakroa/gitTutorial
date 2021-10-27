25d1ade673a102258a26a6b262ba36343b3d2005
Delakroa <katon1986@gmail.com>
2021-10-27 08:39:10 +0300

    переместил в папку

--- autoclicer.py
+++ autoclicer.py
@@ -1,11 +1,12 @@
 import pyautogui as pg
 
-pg.moveTo(115, 931, 2)
-
+# pg.moveTo(115, 931, 2)
+pg.size()
+print(pg.size())
 print(pg.position())  # Shift+F10
 
-pg.move(50, 100, 0.4)
-pg.rightClick(50, 100)
+pg.move(192, 793, 0.4)
+# pg.rightClick(50, 100)
 
 # pg.doubleClick(115, 931)
 # pg.moveTo(588, 600, 2)
